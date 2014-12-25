bf217d44383c9a91eacef134f99ee26e90f6b72b
Eran Rosenthal <eranroz@cs.huji.ac.il>
2014-12-25 12:16:19 +0200

    more general...

--- src/config_default.py
+++ src/config_default.py
@@ -5,6 +5,8 @@ Use this file to define directories that various scripts use.
 __author__ = 'eran'
 import os
 
+GENOME = 'hg19'
+
 # directory with all dnase data as files of { 'chr1': array, 'chr2': array } pickled and zip
 DATA_DIR = os.path.abspath('../data/')
 RAW_DATA_OTHER = os.path.abspath('../raw_other_data')  # backup directory to save original data files
@@ -13,14 +15,14 @@ RES_DIR = os.path.abspath('../results/')  # directory for results
 BIN_DIR = os.path.abspath('../bin/')  # directory for executable of transformers
 PUBLISH_DIR = os.path.abspath('../results/hub/')  # directory accessible to web for publish results
 BED_GRAPH_RESULTS_DIR = os.path.join(RES_DIR, 'open-closed', 'data')  # directory for segmentation
-NCBI_DIR = os.path.join(DATA_DIR, 'data')
+SIGNAL_DIR = os.path.join(DATA_DIR, 'data')  # directory with serialized data (npz format)
 
 # dependencies - use dataDownloader.setup_environment or install.sh in bin directory
 WIG_TO_BIG_WIG = os.path.join(BIN_DIR, 'wigToBigWig')
 BIG_WIG_TO_BED_GRAPH = os.path.join(BIN_DIR, 'bigWigToBedGraph')
 BED_GRAPH_TO_BIG_WIG = os.path.join(BIN_DIR, 'bedGraphToBigWig')
 BED_TO_BIG_BED = os.path.join(BIN_DIR, 'bedToBigBed')
-CHROM_SIZES = os.path.join(BIN_DIR, 'hg19.chrom.sizes')
+CHROM_SIZES = os.path.join(BIN_DIR, '{}.chrom.sizes'.format(GENOME))
 
 
 # can be created by createMeanMarkers
@@ -33,3 +35,6 @@ MODELS_DIR = os.path.join(RES_DIR, "models")
 # publish
 PUBLISH_URL_PATH_HUB = 'http://...'  # accessible url to expose results to genome browser
 TRACK_DESCRIPTION_TEMPALTE = os.path.abspath(os.path.join("data_provider", "trackDescriptionTemplate.html"))
+
+# other configurations
+BIO_DATA_CREDITS = ''  # credit for the data (or the source of the biological data)
